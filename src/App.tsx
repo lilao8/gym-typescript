a02712d7c32e3ff229e652b9172e1401d0482703
Guocheng Li <56323719+lilao8@users.noreply.github.com>
2023-01-21 16:19:13 -0500

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
-      <Footer setSelectedPage={setSelectedPage} />
+      <Footer/>
     </div>
   );
 }
